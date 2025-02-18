da523177f1c845858fc558018071643d7914ef2a
IvDev1706 <144570308+IvDev1706@users.noreply.github.com>
2025-02-18 12:56:16 -0600

    Exponente

--- funciones.py
+++ funciones.py
@@ -1,5 +1,10 @@
-def sum(*nums)->int:
+def sum(*nums)->float:
     sum = 0
     for num in nums:
         sum += num
     return sum
+
+def pow(base, exp)->float:
+    pow = 1
+    for i in range(exp):
+        pow *= base 
