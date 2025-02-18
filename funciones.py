9f66fc0d0499210fb7c5fcb252cd606a42f79bcd
Fisherk2 <fisher_k2@hotmail.com>
2025-02-18 13:12:51 -0600

    Funcion de resta: Se agrega nueva función de resta.

--- funciones.py
+++ funciones.py
@@ -9,3 +9,17 @@ def mult(*nums)->float:
     for num in nums:
         mult *= num
     return mult
+
+def resta (minuendo, sustraendo):
+    """
+    Funcion que resta dos numeros
+    :param minuendo: Numero a restar
+    :param sustraendo: Numero que resta
+    :return: Resultado de la resta
+    :exception Si se intenta restar con otro tipo de valor, devolvera 0
+    """
+    try:
+        return minuendo - sustraendo
+    except TypeError:
+        print(f"No se puede restar {minuendo} con {sustraendo}")
+        return 0
